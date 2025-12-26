229e1641f686ef91d00e9c08a9d8bb5c1c90cf6b
Ashraf3968 <Ashrafwag786@gmail.com>
2025-12-26 23:57:48 +0530

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(500).send("❌ AUTH FILE UPDATED – IF YOU SEE THIS, VERCEL IS USING NEW CODE");
+  res.status(200).send("AUTH FUNCTION WORKING");
 }
